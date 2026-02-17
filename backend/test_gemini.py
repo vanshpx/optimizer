266117252f501f805e40e6028278a96b87367a8b
Sujal Verma <sujalvermakt295@gmail.com>
2026-02-17 20:43:36 +0530

    Delete backend directory

--- backend/test_gemini.py
+++
@@ -1,3 +0,0 @@
-from llm import call_llm
-
-print(call_llm("itinerary to visit goafor 3 days"))
--- backend/test_keys.py
+++
@@ -1,7 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-print("Tavily:", bool(os.getenv("TAVILY_API_KEY")))
-print("Google:", bool(os.getenv("GOOGLE_PLACES_API_KEY")))
